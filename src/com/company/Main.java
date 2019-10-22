9845f19d5aef9a43a30697ccc195db1a80069d65
ahmad-salih <saleh_ahmad@outlook.com>
2019-10-22 21:09:25 +0300

    second line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first line");
+        System.out.println("second line");
     }
 }
